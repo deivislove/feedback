0fdcdf1e21e57f4db9426e42559dd4ee71d90ebc
Davi <davi.hamorim@eaportal.org>
2024-06-11 12:12:51 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,43 @@
-import { ButtonConteiner, CardContainer, IconConteiner } from "./Home.styles";
+import { ButtonConteiner, CardContainer, IconConteiner, ResultConteiner } from "./Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+    let aparecerResultado = true
     return (
-     <CardContainer>
-        <IconConteiner>
-            <img src={iconStarImg} alt="ícone de estrela" />  
-        </IconConteiner>
+        aparecerResultado === false ? (
+        <CardContainer>
+            <IconConteiner>
+                <img src={iconStarImg} alt="ícone de estrela" />  
+            </IconConteiner>
 
-        <h1>Como foi o atendimento?</h1>
-        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+            <h1>Como foi o atendimento?</h1>
+            <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
-        <ButtonConteiner> 
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-        </ButtonConteiner>
+            <ButtonConteiner> 
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+            </ButtonConteiner>
 
-        <button>Enviar</button>
-    </CardContainer>
+            <button>Enviar</button>
+        </CardContainer>
+        ) :  (
+            <CardContainer>
+              <img src={thankYouImg} alt="imagem de agradecimento" />
+      
+              <ResultConteiner>
+                <p>Você selecionou 4 de 5</p>
+              </ResultConteiner>
+      
+              <h1>Obrigado!</h1>
+      
+              <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato! </p>
+            </CardContainer>
+        )
     )
+     
   }
